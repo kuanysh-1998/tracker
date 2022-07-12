a8e8395d06dbf085c0b8d2b8ae385b517b41a8e9
Kuanysh Aptaizhanov <89320116+kuanysh-1998@users.noreply.github.com>
2022-07-12 17:05:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let second = document.querySelector('.second');
+let secondDescription = document.querySelector('.second-footer-description');
+
+second.addEventListener('click', function() {
+    secondDescription.innerText = 'Ограниченная способность поднимать руки и переносить предметы';
+})
